cf33bc11378ca01d429e5f2629f5ba8d48893cd2
Tejas Indulkar <tindulkar@slb.com>
2020-01-21 16:08:56 +0530

    commit 3

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ resource "null_resource" "default" {
     command = "echo first_version"
   }
 }
+resource "null_resource" "default02" {
+  provisioner "local-exec" {
+    # Bootstrap script called with private_ip of each node in the clutser
+    command = "echo second_version"
+  }
+}
